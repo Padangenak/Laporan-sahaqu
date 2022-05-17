c310221aadd3b407faf828569e8820402ce38094
unknown <kakujaa0015@gmail.com>
2022-05-17 22:44:16 +0700

    index blade

---
+++ database/migrations/2022_05_17_123849_report.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Report extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('report', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('participant_id');
+            $table->string('status');
+            $table->string('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('report');
+    }
+}
